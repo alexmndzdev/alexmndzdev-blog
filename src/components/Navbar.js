a2132fddd805597e9e4ba1ff6d3979659b49dc5b
Alejandro Méndez <alejandro.mendez.developer@gmail.com>
2021-12-06 20:58:39 -0600

    Translate labels on navbar and remove github link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,32 +64,20 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                Acerca
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Productos
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Contacto
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Ejemplos de Formulario
               </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
