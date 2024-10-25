8248d993ee313d39a8f4c92a9ffdc495f7fafbc1
amendez <amendez@mikiosko.mx>
2024-10-25 15:26:00 -0600

    Change testimonies by carousel
    TODO: Carousel is not working

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,20 +1,66 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { v4 } from "uuid";
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons
+} from './EmblaCarouselArrowButtons';
+import useEmblaCarousel from 'embla-carousel-react';
 
-const Testimonials = ({ testimonials }) => (
-  <div className="pt-6">
-    {testimonials.map((testimonial) => (
-      <article key={v4()} className="message">
-        <div className="message-body">
-          {testimonial.quote}
-          <br />
-          <cite> – {testimonial.author}</cite>
+const Testimonials = ({ testimonials }) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true });
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi);
+
+  return (
+    <section className="embla">
+      <div className="embla__viewport">
+        <div className="embla__container" ref={emblaRef}>
+          {testimonials.map((testimonial) => (
+            <div key={v4()} className="embla__slide">
+              <div className="card">
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image is-48x48">
+                        <img
+                          src="https://bulma.io/assets/images/placeholders/96x96.png"
+                          alt="Placeholder image"
+                        />
+                      </figure>
+                    </div>
+                    <div className="media-content">
+                      <p className="title is-4">{testimonial.author}</p>
+                      <p className="subtitle is-6">@johnsmith</p>
+                    </div>
+                  </div>
+
+                  <div className="content">
+                    {testimonial.quote}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      </article>
-    ))}
-  </div>
-);
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+      </div>
+    </section>
+  )
+}
 
 Testimonials.propTypes = {
   testimonials: PropTypes.arrayOf(
